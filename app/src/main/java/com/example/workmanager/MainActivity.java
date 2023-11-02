6c89e09201641a6cd7251cea87bae7a21bef0831
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-03 00:37:08 +0300

    Chaining and Monitoring

--- app/src/main/java/com/example/workmanager/MainActivity.java
+++ app/src/main/java/com/example/workmanager/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
@@ -43,5 +45,36 @@ public class MainActivity extends AppCompatActivity {
                 .setInputData(data)
                 .build();
         WorkManager.getInstance(this).enqueue(workRequest);
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.getId()).observe(
+                this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo) {
+                        if (workInfo.getState() == WorkInfo.State.RUNNING){
+                            System.out.println("running");
+                        } else if (workInfo.getState() == WorkInfo.State.SUCCEEDED) {
+                            System.out.println("succeded");
+                        } else if (workInfo.getState() == WorkInfo.State.FAILED) {
+                            System.out.println("failed");
+                        }
+                    }
+                }
+        );
+
+        //WorkManager.getInstance(this).cancelAllWork();
+
+        //Chaining
+        /*
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(RefreshDatabase.class)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .build();
+
+        WorkManager.getInstance(this).beginWith(OneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .enqueue();
+
+         */
     }
 }
